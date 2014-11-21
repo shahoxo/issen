41c39ea306ea6f4c5682befe897e7bc394a817d8
Jun Sitow <jshitou@aiming-inc.com>
2014-11-21 15:33:15 +0900

    add Player class

--- issen/Main.cs
+++ issen/Main.cs
@@ -6,7 +6,10 @@ namespace Issen
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			var player = new Player(hp: 100, strength: 10);
+			Console.WriteLine ("test start");
+			Console.WriteLine ("MaxHp: {0}, Hp: {1}, Strength: {2}", player.MaxHp, player.Hp, player.Strength);
+			Console.WriteLine ("test end");
 		}
 	}
 }

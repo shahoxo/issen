1b0b8ff99917a3a2aa6b7cb711e8d11124fe44c9
Jun Sitow <jshitou@aiming-inc.com>
2014-11-26 17:06:45 +0900

    add support for player's inputs

--- issen/Main.cs
+++ issen/Main.cs
@@ -15,9 +15,17 @@ namespace Issen
 			while(!battle.IsEnded())
 			{
 				Console.WriteLine("---------");
+				if(battle.NextAttackerOf(player))
+				{
+					Console.WriteLine("カードを選択してください");
+					Console.WriteLine("選択可能カード: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+					var card = int.Parse(Console.ReadLine());
+					battle.DecideAttackCard(card);
+				}
 				battle.ForwardTurn();
 				battle.LogLatestTurn();
 			}
+			Console.WriteLine("{0} win!", battle.Winner().Name());
 			Console.WriteLine("battle end");
 		}
 	}
